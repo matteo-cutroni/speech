8c772546a08ba8979ac30b786738678f7d49b7a5
matteo cutroni <cutroni.1918336@studenti.uniroma1.it>
2024-10-14 02:39:04 +0200

    model?

--- tts.py
+++ tts.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-padded = np.load('ljs_tokenized.npy')
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=100):
@@ -52,4 +51,52 @@ class VariancePredictor(nn.Module):
         x = self.dropout(self.ln(x))
         x = x.transpose(1, 2)
         return self.linear(x).squeeze(-1)
+
+
+class LengthRegulator(nn.Module):
+    def __init__(self):
+        super().__init__()
     
+    def forward(self, x, durations):
+        ret = []
+        for i, seq in enumerate(x):
+            expanded_seq = []
+            for j, frame in enumerate(seq):
+                expanded_seq += [frame] * durations[i][j]
+            ret.append(torch.stack(expanded_seq))
+        return nn.utils.rnn.pad_sequence(ret, batch_first=True)
+
+
+class Decoder(nn.Module):
+    def __init__(self, d_model, nhead, num_decoder_layers, dim_feedforward, dropout=0.1):
+        super().__init__()
+        self.positional_encoding = PositionalEncoding(d_model)
+        decoder_layer = nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout)
+        self.transformer_decoder = nn.TransformerEncoder(decoder_layer, num_decoder_layers)
+
+    def forward(self, x):
+        x = self.positional_encoding(x)
+        return self.transformer_decoder(x)
+    
+
+class TtS(nn.Module):
+    def __init__(self, vocab_size, d_model=256, n_mels=80, nhead=4, num_encoder_layers=6, num_decoder_layers=6, dim_feedforward=1024):
+        super().__init__()
+
+        self.encoder = Encoder(vocab_size, d_model, nhead, num_encoder_layers, dim_feedforward)
+        self.duration_predictor = VariancePredictor(d_model)
+        self.length_regulator = LengthRegulator()
+        self.pitch_predictor = VariancePredictor(d_model)
+        self.energy_predictor = VariancePredictor(d_model)
+        self.decoder = Decoder(d_model, nhead, num_decoder_layers, dim_feedforward)
+        self.mel_linear = nn.Linear(d_model, n_mels)
+
+    def forward(self, x, durations, pitch, energy):
+        x = self.encoder(x)
+        x = self.duration_predictor(x)
+        len_reg_output = self.length_regulator(x, durations)
+        pitch_out = self.pitch_predictor(len_reg_output)
+        energy_out = self.energy_predictor(len_reg_output)
+        x = len_reg_output + pitch_out.unsqueeze(-1) + energy_out.unsqueeze(-1)
+        x = self.decoder(x)
+        return self.mel_linear(x)
